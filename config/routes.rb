3e80597de16df26cce195735fa6a174c14fc1f00
nagasho1122 <nagasetopuun1122@icloud.com>
2021-09-02 07:07:03 +0000

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   get '/login', to:'sessions#new'
+  get '/users' , to:'users#index'
   post '/login', to:'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
